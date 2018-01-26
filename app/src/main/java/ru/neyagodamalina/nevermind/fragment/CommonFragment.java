76d44fa33b3b8b5218eee3ec8fe0780e32fce587
Sergey Malinin <neyagodamalina@gmail.com>
2018-01-26 16:59:23 +0300

    Create CommonFragment

---
+++ app/src/main/java/ru/neyagodamalina/nevermind/fragment/CommonFragment.java
@@ -0,0 +1,13 @@
+package ru.neyagodamalina.nevermind.fragment;
+
+import android.support.v4.app.Fragment;
+
+import java.util.Calendar;
+
+/**
+ * Created by developer on 26.01.2018.
+ */
+
+public class CommonFragment extends Fragment {
+    protected Calendar createCalendar = Calendar.getInstance();
+}
--- app/src/main/java/ru/neyagodamalina/nevermind/fragment/CreateTaskFragment.java
+++ app/src/main/java/ru/neyagodamalina/nevermind/fragment/CreateTaskFragment.java
@@ -11,6 +11,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import ru.neyagodamalina.nevermind.R;
 import ru.neyagodamalina.nevermind.business.util.Constants;
 
@@ -18,17 +23,22 @@ import ru.neyagodamalina.nevermind.business.util.Constants;
  * Created by developer on 05.12.2017.
  */
 
-public class CreateTaskFragment extends Fragment {
+public class CreateTaskFragment extends CommonFragment {
     private Button  mCleanTaskTextButton;
     private EditText mEditTaskText;
     private View mViewFragment;
-
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         mViewFragment =  inflater.inflate(R.layout.create_task_fragment, container, false);
+
+        // Покажем время создания этого фрагмента
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+        mEditTaskText = (EditText) mViewFragment.findViewById(R.id.etTask);
+        mEditTaskText.setText(dateFormat.format(this.createCalendar.getTime()));
+
         mCleanTaskTextButton = (Button) mViewFragment.findViewById(R.id.btCleanTaskText);
         mCleanTaskTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
